ebe8ae48479be2c2a1c7f198eb7a9499b7b47ddd
Frank Martinez <frankie.martinez@gmail.com>
2019-04-03 20:39:14 -0700

    AWS Amplify working

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,15 @@
 import React, { Fragment, Component } from 'react';
+import Amplify from 'aws-amplify';
+import awsmobile from '../aws-exports';
+import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
 
-import { connect } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+Amplify.configure(awsmobile);
 
+// import { connect } from 'react-redux';
+// import { BrowserRouter, Route } from 'react-router-dom';
 
-import Navbar from './Navbar';
-import Login from './Login';
+// import Navbar from './Navbar';
+// import Login from './Login';
 
 
 
@@ -14,7 +18,8 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-        <BrowserRouter>
+        <h3>Login</h3>
+        {/* <BrowserRouter>
           <section className='App'>
             
             <Navbar />
@@ -28,7 +33,7 @@ class App extends Component {
             />
   
           </section>
-        </BrowserRouter>
+        </BrowserRouter> */}
       </Fragment>
     );
   }
@@ -36,11 +41,13 @@ class App extends Component {
 
 
 
-const mapStateToProps = state => ({
-  ...state  // This brings in all of state store. you can be explicit as well.
-})
+// const mapStateToProps = state => ({
+//   ...state  // This brings in all of state store. you can be explicit as well.
+// })
 
-export default connect(
-    mapStateToProps, 
-    {  }    // Bring in action here. Remember to import it
-)(App);
+// export default connect(
+//     mapStateToProps, 
+//     {  }    // Bring in action here. Remember to import it
+// )(App);
+
+export default withAuthenticator(App, true)
