c03ca4afb0512029d2c4f532c9798687f430d9cc
Peter Murphy <peter.francis.murphy@gmail.com>
2019-04-09 20:14:52 -0400

    Rewrite actions and reducers

--- client/src/stateTree/actions/index.js
+++ client/src/stateTree/actions/index.js
@@ -1,29 +1,33 @@
-import axios from 'axios';
+import Axios from "axios"
 
-export const GET_START = 'GET_START';
-export const GET_SUCCESS = 'GET_SUCCESS';
-export const GET_FAIL = 'GET_FAIL';
+export const GET_START = "GET_START"
+export const GET_SUCCESS = "GET_SUCCESS"
+export const GET_FAIL = "GET_FAIL"
+export const SET_PROPERTY_ADDRESS = "SET_PROPERTY_ADDRESS"
 
+const { SERVER_BASE_URL } = process.env
 
-
-
-export const getNotes = () => dispatch => {
-    dispatch({
-        type: GET_START
+export const getQuestions = () => dispatch => {
+  dispatch({
+    type: GET_START
+  })
+  Axios.get(SERVER_BASE_URL + "/api/questions")
+    .then(response => {
+      console.log({ response })
+      dispatch({
+        type: GET_SUCCESS,
+        payload: response.data
+      })
     })
-    axios
-      .get('https://fe-notes.herokuapp.com/note/get/all')
-      .then(response => {
-        dispatch({
-            type: GET_SUCCESS,
-            payload: response.data,
-        })
-    })
-      .catch(error => {
-        dispatch({
-            type: GET_FAIL,
-            payload: error.message,
-        })
+    .catch(error => {
+      dispatch({
+        type: GET_FAIL,
+        payload: error.message
+      })
     })
 }
-  
+
+export const setPropertyAddress = address => ({
+  type: SET_PROPERTY_ADDRESS,
+  payload: address
+})
